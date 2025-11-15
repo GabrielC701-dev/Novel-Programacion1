0b36bed5d1c1ce66167696c5aa890270f7f3ff00
GabrielC701-dev <gabriel.caceress020@gmail.com>
2025-11-15 18:04:10 -0400

    class v0.01

--- principal.ts
+++ principal.ts
@@ -1,3 +1,62 @@
 import { leerNumero } from "./funciones";
+import * as readlineSync from "readline-sync";
+import { styleText } from "util";
 
-//probando xd
+export class Protagonista {
+  nombre: string;
+  rutaActual: string = "";
+  estado: any = [];
+
+  constructor(nombre: string) {
+    this.nombre = nombre;
+  }
+
+  elegir(opciones: string[]) {
+    const index = readlineSync.keyInSelect(opciones, "Elige una opción:");
+    return index;
+  }
+}
+
+export class Personaje {
+    nombre: string;
+    color: string;
+  constructor(n: string, c: string) {
+    this.nombre = n;
+    this.color = c;
+  }
+
+  hablar(texto: string) {
+    const nombrecolor = (this.color as any, `${this.nombre}: `); 
+    console.log(nombrecolor + texto);
+  }
+}
+
+export class Narrador {
+  narrar(texto: string) {
+    console.log('red',texto)
+  }
+}
+
+class Alex extends Personaje {
+  constructor() {
+    super("Alex", "yellow");
+  }
+}
+
+class Maya extends Personaje {
+  constructor() {
+    super("Maya", "green");
+  }
+}
+
+class Ramirez extends Personaje {
+  constructor() {
+    super("Ramírez", "red");
+  }
+}
+
+class LaVoz extends Personaje {
+  constructor() {
+    super("La Voz", "blue");
+  }
+}
